97f3961c38ed077b6138c7453182c24f6272cae5
Matheusoct <161069867+Matheusoct@users.noreply.github.com>
2024-09-24 14:12:08 -0300

    Add files via upload

---
+++ Atividades Uc15/Carrinho_de_compraJS.js
@@ -0,0 +1,32 @@
+let carrinho = [];
+let total = 0.0;
+
+function adicionarAoCarrinho(nome,preco){
+    carrinho.push({nome:nome, preco: preco});
+    atualizarCarrinho();
+}
+
+function removeDoCarrinho(index){
+    carrinho.splice(index, 1);
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho(){
+    const carrinhoElement = document.getElementById('carrinho');
+    const totalElement = document.getElementById('total');
+
+    carrinhoElement.innerHTML='';
+    total = 0;
+
+    carrinho.forEach((item, index) => {
+        total +=item.preco;
+        carrinhoElement.innerHTML +=`
+            <li>${item.nome} - R$ ${item.preco.toFixed(2)}
+                <button onclick="removeDoCarrinho(${index})">Remover</button>
+            </li>`;
+            
+    });
+
+    totalElement.innerText= `Total: R$ ${total.toFixed(2)}`;
+
+}
